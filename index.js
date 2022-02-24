624ccfb7171093de7a6829f4ef2fcede37137754
MattiaRiola <s280169@studenti.polito.it>
2022-02-24 15:25:35 +0100

    feat: add example and read input languages #1

--- index.js
+++ index.js
@@ -3,19 +3,54 @@ import alfy from 'alfy';
 import wr from 'wordreference-api';
 
 
+
 var data = alfy.input;
-wr(data, 'en', 'it').then((wrResult) => {
-	var results = []
-	for (let index1 = 0; index1 < wrResult.translations.length; index1++) {
-		const outerTranslation = wrResult.translations[index1];
-		for (let index2 = 0; index2 < outerTranslation.translations.length; index2++) {
-			const innerTranslation = outerTranslation.translations[index2];
-			results.push({
-				title: innerTranslation.from,
-				subtitle: innerTranslation.to
-			});
-		}
-	}
-	
-	alfy.output(results);
-});
+var from = data.substring(0, 2);
+var to = data.substring(2, 4);
+let fromText = data.substring(5);
+if (false) {
+	alfy.output([{ title: "wrong languages: " + from + '-' + to }])
+} else {
+	wr(fromText, from, to).then((wrResult) => {
+		var results = []
+		if (wrResult.translations.length == 0)
+			results.push({ title: 'translation not found' });
+		else
+			for (let index1 = 0; index1 < wrResult.translations.length; index1++) {
+				const outerTranslation = wrResult.translations[index1];
+				results.push({
+					title: '~~~~~~~~~~~' + outerTranslation.title + '~~~~~~~~~~~'
+				})
+				for (let index2 = 0; index2 < outerTranslation.translations.length; index2++) {
+					const innerTranslation = outerTranslation.translations[index2];
+					if (innerTranslation.example.from.length > 0) {
+						results.push(
+							{
+								title: innerTranslation.from + ' → ' + innerTranslation.to,
+								subtitle: 'examples:'
+							}
+						)
+						for (let i = 0; i < innerTranslation.example.from.length; i++) {
+							const exFrom = innerTranslation.example.from[i];
+							const exTo = innerTranslation.example.to[i];
+							results.push(
+								{
+									title: exFrom,
+									subtitle: exTo
+								}
+							)
+
+						}
+						results.push({ title: ".........................................................................................." })
+					}
+					else
+						results.push({
+							title: innerTranslation.from + ' → ' + innerTranslation.to,
+							// subtitle: 'type: ' + innerTranslation.from + ' → ' + innerTranslation.to
+						});
+				}
+			}
+
+		alfy.output(results);
+	});
+}
